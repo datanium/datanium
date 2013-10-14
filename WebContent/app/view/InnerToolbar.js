496718c460a9b5d2dc1424cf71c578f7c85a034f
benitoro <nicepuffy@gmail.com>
2013-10-14 15:22:50 +0800

    Revert "Revert "minor changes""
    This reverts commit 55af2f95b2f13432f1e855b2b385af2351a0ef22.

--- WebContent/app/view/InnerToolbar.js
+++ WebContent/app/view/InnerToolbar.js
@@ -5,33 +5,45 @@ Ext.define('Datanium.view.InnerToolbar', {
 	alias : 'widget.inner-toolbar',
 	shadow : 'drop',
 	shadowOffset : 10,
-	minHeight : 35,
+	minHeight : 38,
 	padding : '0 5',
 	items : [ {
-		scale : 'small',
+		scale : 'medium',
 		tooltip : 'Run Query',
 		tooltipType : 'title',
-		html : '<i class="icon-play"></i>',
+		html : '<i class="icon-play-sign icon-2x"></i>',
 		action : 'manual-run'
-	}, '-', {
-		scale : 'small',
+	}, {
+		xtype : 'tbseparator',
+		height : 14,
+		margins : '0 0 0 1'
+	}, {
+		scale : 'medium',
 		tooltip : 'Auto Run',
 		tooltipType : 'title',
-		html : '<i class="icon-forward"></i>',
+		html : '<i class="icon-play-circle icon-2x"></i>',
 		action : 'auto-run',
 		enableToggle : true,
 		pressed : true
-	}, '-', {
-		scale : 'small',
+	}, {
+		xtype : 'tbseparator',
+		height : 14,
+		margins : '0 0 0 1'
+	}, {
+		scale : 'medium',
 		tooltip : 'Export Report',
 		tooltipType : 'title',
-		html : '<i class="icon-arrow-down"></i>',
+		html : '<i class="icon-download icon-2x"></i>',
 		action : 'export'
-	}, '-', {
-		scale : 'small',
+	}, {
+		xtype : 'tbseparator',
+		height : 14,
+		margins : '0 0 0 1'
+	}, {
+		scale : 'medium',
 		tooltip : 'Clear Selection',
 		tooltipType : 'title',
-		html : '<i class="icon-remove"></i>',
+		html : '<i class="icon-remove-circle icon-2x"></i>',
 		action : 'clear'
 	} ]
 });
