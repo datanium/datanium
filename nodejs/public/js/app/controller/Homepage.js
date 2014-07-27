15d98c7639a88c5295645f01d346cb57882ed2bd
benitoro <nicepuffy@gmail.com>
2014-07-27 22:38:59 +0800

    grid column format improvement

--- nodejs/public/js/app/controller/Homepage.js
+++ nodejs/public/js/app/controller/Homepage.js
@@ -7,9 +7,6 @@ Ext.define('Datanium.controller.Homepage', {
 	init : function() {
 		this.control({
 			'viewport reporttemplate' : {},
-			'leftpanel > cubecombo' : {
-				select : this.loadTrees
-			},
 			'leftpanel > searchcombo' : {
 				select : this.addIndicator
 			},
@@ -56,41 +53,6 @@ Ext.define('Datanium.controller.Homepage', {
 			'inner-toolbar > button[action=manual-run]' : {
 				click : function(btn) {
 					this.getController('GridController').generateRpt(true);
-				}
-			}
-		});
-	},
-
-	loadTrees : function(combobox, newValue, oldValue, eOpts) {
-		var cubeInfoStore = this.getStore('CubeInfos');
-		var leftpanel = Datanium.util.CommonUtils.getCmpInActiveTab('leftpanel');
-		var mask = new Ext.LoadMask(leftpanel, {
-			msg : "Loading..."
-		});
-		mask.show();
-		cubeInfoStore.load({
-			scope : this,
-			params : {
-				cubeName : combobox.getValue()
-			},
-			callback : function(records, operation, success) {
-				if (success) {
-					var tmpstore = records[0];
-					var dimensionTree = Datanium.util.CommonUtils.getCmpInActiveTab('dimensionTree');
-					var measureTree = Datanium.util.CommonUtils.getCmpInActiveTab('measureTree');
-					var dimensionData = {};
-					var measureData = {};
-					dimensionData.children = tmpstore.data.dimensions;
-					dimensionTree.store.setRootNode(dimensionData);
-					measureData.children = tmpstore.data.measures;
-					measureTree.store.setRootNode(measureData);
-					Datanium.GlobalData.qubeInfo.dimensions = tmpstore.data.dimensions;
-					Datanium.GlobalData.qubeInfo.measures = tmpstore.data.measures;
-					Datanium.util.CommonUtils.getCmpInActiveTab('elementPanel').fireEvent('refreshElementPanel');
-					mask.destroy();
-				} else {
-					console.log('cube loading failed');
-					mask.destroy();
 				}
 			}
 		});
