ddd440bdde8e070df900392df7ba9111be13f1ba
benitoro <nicepuffy@gmail.com>
2014-10-03 23:25:50 +0800

    port

--- nodejs/app.js
+++ nodejs/app.js
@@ -22,7 +22,7 @@ var nocache = function(req, res, next) {
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
