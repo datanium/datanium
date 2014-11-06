786603d8454f08b35384a1c7415004b851ebe537
benitoro <nicepuffy@gmail.com>
2014-11-07 01:13:41 +0800

    load wdi chinese data script enhancement

--- scripts/data_load_worldbank_wdi.py
+++ scripts/data_load_worldbank_wdi.py
@@ -14,6 +14,7 @@ class Static:
     request_url_indicator_zh = 'http://api.worldbank.org/zh/indicators'
     request_url_rowdata_zh = 'http://api.worldbank.org/zh/countries/all/indicators/'
     output_folder = '/Users/Puffy/Works/data_output'
+    dataset_folder = '/20141106'
     database_name = 'datanium'
     indicator_col_name = 'indicator_new'
     dataset_col_name = 'dataset_new'
@@ -111,16 +112,10 @@ def load_indicators_to_mongo_zh(is_incremental):
     print("total records: " + str(indicator_col.count()))
     print("total time cost: " + str(round(timeit.default_timer() - all_start)) + 's')
 
-def load_rowdata_to_mongo_zh(is_incremental):
-    print("start loading row data(zh) from JSON file to MongoDB...")
+def load_rowdata_to_json_zh():
+    print("start loading row data(zh) to JSON file...")
     all_start = timeit.default_timer()
     static = Static()
-    
-    client = MongoClient(static.mongo_url, static.mongo_port)
-    db = client[static.database_name]
-    dataset_col = db[static.dataset_col_name]
-    if not is_incremental:
-        dataset_col.drop()
     
     f = io.open(static.output_folder + '/worldbank_wdi_indicators_zh.json', 'r', encoding='utf8')
     json_str = f.readline()
@@ -130,10 +125,9 @@ def load_rowdata_to_mongo_zh(is_incremental):
     country_dict = json.loads(json_str)
     f.close()
 
-    ## dataset_array = []
     counter = []
-    mapfunc = partial(load_data_by_indicator, counter=counter, dataset_col=dataset_col, country_dict=country_dict, all_start=all_start)
-    pool = ThreadPool(10)
+    mapfunc = partial(load_data_by_indicator, counter=counter, country_dict=country_dict, all_start=all_start)
+    pool = ThreadPool(20)
     pool.map(mapfunc, indicator_array)
     pool.close() 
     pool.join()
@@ -141,9 +135,9 @@ def load_rowdata_to_mongo_zh(is_incremental):
     print("All the threads are completed. Total number is " + str(len(counter)) + "\n")
     print("total time cost: " + str(round(timeit.default_timer() - all_start)) + 's')
 
-def load_data_by_indicator(indicator, counter, dataset_col, country_dict, all_start):
+def load_data_by_indicator(indicator, counter, country_dict, all_start):
     static = Static()
-    ## dataset_by_indicator = []
+    dataset_by_indicator = []
     page_size = 20000
     page_no = 1
     r_params = {'date': '1960:2013', 'format': 'json', 'per_page': 10, 'page': 1}
@@ -152,7 +146,7 @@ def load_data_by_indicator(indicator, counter, dataset_col, country_dict, all_st
     return_obj = json.loads(r.text)
     page_info = return_obj[0]
     total_size = page_info['total']
-    print(">>>" + indicator['name'] + " total " + str(total_size) + "\n")
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()) + " >>> " + indicator['name'] + " total " + str(total_size) + "\n")
     ## print('page size: ' + str(page_size))
 
     while page_no <= round(total_size/page_size, 0):
@@ -169,23 +163,75 @@ def load_data_by_indicator(indicator, counter, dataset_col, country_dict, all_st
                     if value is not None:
                         value = float(res['value'])
                     dataset_rec = {'country': res['country']['value'], 'region': region, 'year': int(res['date']), indicator_key: value, 'load_key': 'WDI_ZH' + str(time.strftime("%Y%m%d"))}
-                    ## dataset_by_indicator.append(dataset_rec)
+                    dataset_by_indicator.append(dataset_rec)
                     counter.append(1)
-                    pk = dataset_col.insert(dataset_rec)
+                    ## pk = dataset_col.insert(dataset_rec)
         page_no += 1
 
-    ## directory = static.output_folder + '/' + str(time.strftime("%Y%m%d"))
-    ## if not os.path.exists(directory):
-    ##     os.makedirs(directory)
-    ## f = io.open(directory + '/WDI_' + indicator_key + '.json', 'w', encoding='utf8')
-    ## json.dump(dataset_by_indicator, f, ensure_ascii=False)
-    ## f.close()
+    if len(dataset_by_indicator) > 0:
+        directory = static.output_folder + '/' + str(time.strftime("%Y%m%d"))
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        f = io.open(directory + '/WDI_' + indicator_key + '.json', 'w', encoding='utf8')
+        json.dump(dataset_by_indicator, f, ensure_ascii=False)
+        f.close()
     
-    print(indicator['id'] + '/' + indicator['name'] + " time cost: " + str(round(timeit.default_timer() - all_start)) + "s. " + "total record number: " + str(len(counter)) + "\n")
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()) + " >>> " + indicator['id'] + '/' + indicator['name'] + " time cost: " + str(round(timeit.default_timer() - all_start)) + "s. " + "total record number: " + str(len(counter)) + "\n")
+
+def load_rowdata_to_mongo_zh(is_incremental):
+    print("start loading row data(zh) from JSON file to MongoDB...")
+    all_start = timeit.default_timer()
+    static = Static()
+    
+    client = MongoClient(static.mongo_url, static.mongo_port)
+    db = client[static.database_name]
+    dataset_col = db[static.dataset_col_name]
+    if not is_incremental:
+        dataset_col.drop()
+    
+    f = io.open(static.output_folder + '/worldbank_wdi_indicators_zh.json', 'r', encoding='utf8')
+    json_str = f.readline()
+    indicator_array = json.loads(json_str)
+    f.close()
+
+    ## dataset_array = []
+    counter = []
+    mapfunc = partial(insert_by_indicator, counter=counter, dataset_col=dataset_col, all_start=all_start)
+    pool = ThreadPool(10)
+    pool.map(mapfunc, indicator_array)
+    pool.close() 
+    pool.join()
+    
+    print("All the threads are completed. Total number is " + str(len(counter)) + "\n")
+    print("total time cost: " + str(round(timeit.default_timer() - all_start)) + 's')
+
+def insert_by_indicator(indicator, counter, dataset_col, all_start):
+    static = Static()
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()) + " >>> " + indicator['name'] + "\n")
+    
+    indicator_key = indicator['id'].replace('.', '_') + '_ZH'
+    json_str = None
+    try:
+        f = io.open(static.output_folder + static.dataset_folder + '/WDI_' + indicator_key + '.json', 'r', encoding='utf8')
+        json_str = f.readline()
+    except FileNotFoundError:
+        print(indicator['name'] + ' has no data.')
+    if json_str is not None:
+        dataset_array = json.loads(json_str)
+        for dataset_rec in dataset_array:
+            doc = dataset_col.find_one({"year": dataset_rec['year'], "region": dataset_rec['region'], "country": dataset_rec['country']})
+            if doc is not None:
+                dataset_col.update({'_id': doc['_id']}, {'$set':{indicator_key: dataset_rec[indicator_key]}})
+            else:
+                pk = dataset_col.insert(dataset_rec)                
+            counter.append(1)
+    
+    print(time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()) + " >>> " + indicator['id'] + '/' + indicator['name'] + " time cost: " + str(round(timeit.default_timer() - all_start)) + "s. " + "total record number: " + str(len(counter)) + "\n")
 
 if __name__ == '__main__':
     ## load_countries_to_json_zh()
     ## load_indicators_to_json_zh()
     ## load_indicators_to_mongo_zh(False)
+    ## load_rowdata_to_json_zh()
     load_rowdata_to_mongo_zh(False)
     
